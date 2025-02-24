d61473357efc38ff7094f7657f4d06106e7c56b4
ThiagoFragaK <thiagofraga@proton.me>
2025-02-24 19:47:26 -0300

    Add: Product List compoent

---
+++ components/products/ProductsListComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>Welcome to the Homepage</h2>
+    </div>
+</template>
+
+<script>
+    import FakerService from "@/services/FakerService.js";
+    export default {
+        methods: {
+            getProducts() {
+                FakerService.getAllProducts()
+                    .then((response) => {
+                        console.log(response);
+                    });
+            },
+            getProductByID(productID) {
+                FakerService.getProductByID(productID)
+                    .then((response) => {
+                        console.log(response);
+                    });
+            },
+        },
+        mounted() {
+            this.getProducts();
+            this.getProductByID(5);
+        }
+    }
+</script>
